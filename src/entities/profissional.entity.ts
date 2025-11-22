0658a604c98990c1a54d5fa39ddce983a72ac297
Jéssica Patrocínio <jessicapatrocinio0902@gmail.com>
2025-11-22 14:05:39 -0300

    Update profissional.entity.ts

--- src/entities/profissional.entity.ts
+++ src/entities/profissional.entity.ts
@@ -1,5 +1,6 @@
 import { PrismaClient } from "@prisma/client"
-
+import Profissional from "../entities/profissional.entity.js" 
+  
 const prisma = new PrismaClient()
 
 export default prisma.profissional
