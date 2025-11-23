6c9f32dff5b8280a96cefcec150780816c983f6a
Jéssica Patrocínio <jessicapatrocinio0902@gmail.com>
2025-11-23 12:04:06 -0300

    servicos p profissional atualizados

--- src/services/profissional.service.ts
+++ src/services/profissional.service.ts
@@ -1,21 +1,21 @@
 import * as profissionalRepository from "../repositories/profissional.entity.js"
+import type { ProfissionalBody } from "../controllers/profissional.c.js";
+import Profissional from '../entities/profissional.entity.js';
 
-export async function createProfissional(){
+
+export async function createProfissional(data: ProfissionalBody){
 
 }
-
 export async function getAllProfissionais() {
 
 }
 
-export async function getProfissionalById(){
-    
-}
+export async function getProfissionalById(id: number): Promise<Profissional | null>{}
 
-export async function updateProfissional(){
+export async function updateProfissional(id: number, data: Partial<ProfissionalBody>){
 
 }
 
-export async function deleteProfissional(){
+export async function deleteProfissional(id: number){
     
 }
