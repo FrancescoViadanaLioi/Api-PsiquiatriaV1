08111689386f49629ac5834677406bf1ed6654a0
Jéssica Patrocínio <jessicapatrocinio0902@gmail.com>
2025-11-24 20:49:46 -0300

    Refactor profissional service to use repository methods

--- src/services/profissional.service.ts
+++ src/services/profissional.service.ts
@@ -1,35 +1,25 @@
-import type { ProfissionalBody } from "../controllers/profissional.c.js";
-import { PrismaClient, type Profissional } from '@prisma/client';
-const prisma = new PrismaClient();
-
-export async function createProfissional(data: ProfissionalBody): Promise<Profissional> {
-    const novoProfissional = await prisma.profissional.create({
-        data, 
-    });
-    return novoProfissional;
-}
-export async function getAllProfissionais(): Promise<Profissional[]> {
-    return prisma.profissional.findMany();
+import * as profissionalRepository from '../repositories/profissional.repository.js'
+export interface Profissional {
+    nome: string;
+    especialidade: string;
+    registroConselho: string;
 }
 
-export async function getProfissionalById(id: number): Promise<Profissional | null> {
-    const profissional = await prisma.profissional.findUnique({
-        where: { id },
-    });
-    return profissional;
+export async function createProfissional(data: Profissional) {
+    return await profissionalRepository.create(data)
 }
 
-export async function updateProfissional(id: number, data: Partial<ProfissionalBody>){
-const profissionalAtualizado = await prisma.profissional.update({
-        where: { id },
-        data,
-    });
-    return profissionalAtualizado;
+export async function getAllProfissionais() {
+    return await profissionalRepository.findAll()
 }
 
-export async function deleteProfissional(id: number): Promise<Profissional> {
-    const profissionalDeletado = await prisma.profissional.delete({
-        where: { id },
-    });
-    return profissionalDeletado;
+export async function getProfissionalById(id: number) {
+    return await profissionalRepository.findById(id)
+}
+export async function updateProfissional(id: number, data: Partial<Profissional>) {
+    return await profissionalRepository.update(id, data) 
+}
+
+export async function deleteProfissional(id: number) {
+    return await profissionalRepository.remove(id)
 }
